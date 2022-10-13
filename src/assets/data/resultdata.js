a2f2c51f15da3957084a230a2ede7cc091306ad9
narsyaj <narsyaj7@gmail.com>
2022-10-13 23:23:08 +0900

    add result page

---
+++ src/assets/data/resultdata.js
@@ -0,0 +1,12 @@
+import RussianBlue from '../cat/cat00.jpg'
+
+export const ResultData = [
+    {
+        id: 1,
+        name: '러시안 블루',
+        best: 'ESTJ',
+        desc: '겁이 많아요. 워낙 온순한 성격에 조용한 고양이라..',
+        image: `${RussianBlue}`,
+    },
+
+]
